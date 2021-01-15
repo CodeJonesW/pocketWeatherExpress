3e781c2f3293da79b16ef928398e1ebe230487c2
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-01-15 01:06:02 -0500

    get weather by city

--- routes/weather.js
+++ routes/weather.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios')
 const apiKey = process.env.apiKey
-
+const cors = require("cors")
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.send({"message": "Hello"})
+  res.send("pocketWeather API")
 });
 
-router.get('/api/currentWeather/:city', async (req, res) => {
+
+router.get('/api/currentWeather/:city', cors(), async (req, res) => {
   let city = await req.params.city
   let currentWeatherData = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`)
   let fiveDayForecastData = axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
